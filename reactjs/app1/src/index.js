a5cad414000df7aa10fd10cfcfb07a3b8fe6e97d
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-09-14 17:07:29 +0530

    day 2

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// day 1
+// example 1
+// const output = <div className='container'><div className='row'><div className='col-12'><h1>First Example in React</h1><hr/><p>this is first line in react <br/> this is second line in react</p></div></div></div>
+
+//example 2(use of expression)
+let number1 = 10;
+let number2 = 3;
+let output = <div className='container'><div className='row'><div className='col-12'><h1>Expression in javascript </h1> <hr/> <b>we can display variables & we can use Expression in javascript <br/></b>{number1} + {number2} = {number1 + number2} <br/> {number1} - {number2} = {number1 - number2}</div></div></div>
+root.render(output);
