0929752b06e49c947fcbd832609b456391d69b3c
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-12-20 16:18:52 +0530

    apidemo

--- nodejs/database/apidemo.js
+++ nodejs/database/apidemo.js
@@ -65,8 +65,35 @@ app.delete("/category/:categoryid",function(request,response){
     var sql = "delete from category where id=" + request.params.categoryid;
     mysql.con.query(sql,function(error,result,fields){
         ReturnError(error,response);
-        response.json({message:'category has been deleted successfully'});
+        if(result.affectedRows==0)
+            response.json({message:'category not found'});
+        else
+            response.json({message:'category has been deleted successfully'});
     });
-    
 })
+//http://127.0.0.1:5000/category/calendar/calendar.jpg
+app.get("/category/:title/:photo",function(request,response){
+    var title = request.params.title;
+    var photo = request.params.photo;
+    var sql = `insert into category (title,photo) values ('${title}','${photo}')`;
+    mysql.con.query(sql,function(error,result,fields){
+        ReturnError(error,response);
+        response.json({message:'new category has been added successfully'});
+    });
+});
+
+//http://127.0.0.1:5000/category/mouse_pad/mouse_pad.jpg/35
+app.get("/category/:title/:photo/:id",function(request,response){
+    var title = request.params.title;
+    var photo = request.params.photo;
+    var id = request.params.id;
+    var sql = `update category set title='${title}', photo='${photo}' where id=${id}`
+    mysql.con.query(sql,function(error,result,fields){
+        ReturnError(error,response);
+        if(result.affectedRows==0)
+            response.json({message:'category not found'});
+        else
+            response.json({message:'category has been updated successfully'});
+    });
+});
 app.listen(5000);
