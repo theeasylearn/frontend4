170d106d203cf15aefda19042cda1809d5af22b3
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-01-01 15:38:17 +0530

    api

--- nodejs/mongodb/api.js
+++ nodejs/mongodb/api.js
@@ -10,6 +10,12 @@ app.use(bodyParser.json())
 app.get("/students",(request,response) => student.getAllStudent(request,response));
 
 //http://127.0.0.1:5000/student/ShivKumar/IsconCity
+/* set content type : application/json for sending data in json format 
+   put json object in body part like {"name":"bimal","city":"baroda"}
+
+   set content type : application/application/x-www-form-urlencoded  for sending data in query string format 
+   put query string in body part name=chintan&city=channai
+*/
 app.post("/student",(request,response) => student.InsertStudent(request,response));
 
 app.listen(5000);
--- nodejs/mongodb/student_api.js
+++ nodejs/mongodb/student_api.js
@@ -10,7 +10,8 @@ let  getAllStudent  = function (request,response)
         {
             var nodejs = database.db(common.DATABASE_NAME);
             var byname = { name : -1}; // use 1 for asending order -1 for descending order 
-            nodejs.collection("student").find({},{}).sort(byname).toArray(function(error,student){
+            var Condition = {"name":{$ne:null}}
+            nodejs.collection("student").find(Condition,{}).sort(byname).toArray(function(error,student){
                 if(error!=null)
                 {
                     console.log(error.errmsg);
