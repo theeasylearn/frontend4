bb96dcbf15d9ab2e2fc2219b1b266a6bc9fe0e20
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-01-11 16:49:40 +0530

    pug day 2

--- nodejs/pug/lesson1.js
+++ nodejs/pug/lesson1.js
@@ -7,5 +7,25 @@ app.get("/one",function(request,response){
    response.render("one") 
 });
 
+app.get("/two",function(request,response){
+   response.render("two") 
+});
+
+app.get("/three",function(request,response){
+   response.render("three") 
+});
+
+app.get("/login",function(request,response){
+   response.render("login") 
+});
+
+app.get("/contact",function(request,response){
+   response.render("contact",{
+      name:'the easylearn academy',
+      mobile: 9662512857,
+      email : 'theeasylearn@gmail.com'
+   });
+});
+
 app.listen(5000);
 console.log("server is ready");
