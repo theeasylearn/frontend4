ac247d4dce082efe0514da3fe1b6d67cb67ce6e3
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-01-12 16:42:54 +0530

    loop1

--- nodejs/pug/lesson1.js
+++ nodejs/pug/lesson1.js
@@ -26,6 +26,32 @@ app.get("/contact",function(request,response){
       email : 'theeasylearn@gmail.com'
    });
 });
+app.get("/contact2",function(request,response){
+   response.render("contact",{
+      mobile: 9662512857,
+      email : 'theeasylearn@gmail.com'
+   });
+});
 
+app.get("/loop1",function(request,response){
+   response.render("loop1",{
+      countries : ['India','Australia','Brazil','US','UK']
+   });
+});
+
+app.get("/loop2",function(request,response){
+   response.render("loop2",{
+      languages : ['Hindi','Gujarati','Marathi','Tamil','Telugu']
+   });
+});
+
+app.get("/loop3",function(request,response){
+   response.render("loop3",{
+      book : {name:'The Secret',author:'Unknown',pages:200,price:500}
+   });
+});
+app.get("/loop4",function(request,response){
+   response.render("loop4");
+});
 app.listen(5000);
 console.log("server is ready");
