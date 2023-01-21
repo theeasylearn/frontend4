2fdb3b4490ce9d88f4ffea8298c81da7e2f019f0
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-01-21 16:11:35 +0530

    complete site using node and pug

--- nodejs/site/website.js
+++ nodejs/site/website.js
@@ -5,7 +5,19 @@ app.set("view engine","pug");
 app.set("views","pages");
 
 app.get("/",function(request,response){
-    response.render("template")
+    response.render("home")
+});
+app.get("/aboutus",function(request,response){
+    response.render("aboutus")
+});
+app.get("/products",function(request,response){
+    response.render("products")
+});
+app.get("/services",function(request,response){
+    response.render("services")
+});
+app.get("/contactus",function(request,response){
+    response.render("contactus")
 });
 app.listen(5000);
 console.log("server is ready");
